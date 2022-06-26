c144ba9d19141b65c6cf464afec249b0abc541bd
Franko777 <khubul.george@inbox.ru>
2022-06-26 11:57:01 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/thegithubclient/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.gb.thegithubclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
